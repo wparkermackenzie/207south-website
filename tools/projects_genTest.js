371cd47cd7b28c4a7404d8c7224db87b90a57348
Parker Mackenzie <wparkermackenzie@outlook.com>
2021-07-02 18:24:04 -0400

    Work in Progress creating a tool to generate the projects test

--- tools/projects_genTest.js
+++ tools/projects_genTest.js
@@ -30,31 +30,56 @@ let projects =
 function handleGenerate(argv) {
   const nTopics = argv.topics;
   const nProjects = argv.projects;
+  const strLenMax_nChars = 128;
   if(undefined === nTopics || undefined === nProjects) {
     console.error('Error: nTopics and nProjects need to be defined');
     process.exit(1);
   }
 
   for(let t = 0; t < nTopics; t++) {
+    let tn = genString(strLenMax_nChars);
+    let td = genString(strLenMax_nChars);
     let topic = {
-      displayName: 'a' + t,
-      displayDescription: 'b' + t,
+      displayName: tn,
+      displayDescription: td,
       projects:[]
     };
 
     for(let p = 0; p < nProjects; p++ )
     {
+      let pn = genString(strLenMax_nChars);
+      let pd = genString(strLenMax_nChars);
+      let pi = genString(strLenMax_nChars);
+      let pp = genString(strLenMax_nChars);
       let project = {
-        displayName: 'c' + t + p,
-        displayDescription: 'd' + t + p,
-        displayImage: 'e' + t + p,
-        page: 'f' + t + p
+        displayName: pn,
+        displayDescription: pd,
+        displayImage: pi,
+        page: pp
       }
       topic.projects.push(project);
     }
     projects.topics.push(topic);
   }
-  console.log( JSON.stringify(projects, null, 2) );
+  
+  test = `
+  test('Projects: Happy Path', () => {
+    const json = ${JSON.stringify(projects, null,2)};
+    const projects = new Projects(json);
+    let nEntries = 0;
+    for(let entry of projects) {
+      nEntries++;
+      // find project here...loop through topics to find dn & dd 
+      // loop through projects to find
+      // >>> or already know order just fill it all in given 
+      //     topicNo = nEntries
+      expect(entry.description).toBe(projects.topics)
+
+    }
+  });
+  `;
+
+  //console.log( JSON.stringify(projects, null, 2) );
 }
 
 // Set a version of the program.
@@ -90,6 +115,19 @@ yargs.command({
 .argv;
 
 yargs.parse();
+
+function genString(nCharsMax)
+{
+  let r='';
+  const nChars = Math.floor(Math.random() * nCharsMax);
+  for( let i = 0; i < nChars; i++) {
+    c = String.fromCharCode(Math.floor(Math.random() * 95)+32);
+    if( c === '"') {
+      c = '\"';
+    }
+  }
+  return(r);
+}
 
 
 
